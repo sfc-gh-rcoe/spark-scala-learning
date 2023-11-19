94cd35f22c5d299d39e578b202efc26c09441343
Ronald Coe <ronald.coe@snowflake.com>
2023-11-19 08:37:07 -0500

    Modified file write logic for VSCode environment

--- generate-data/src/main/scala/gen-data.scala.scala
+++ generate-data/src/main/scala/gen-data.scala.scala
@@ -1,8 +1,10 @@
 import java.io.PrintWriter
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import pureconfig._
 import pureconfig.generic.auto._
 
-object Main {
+object MainData {
 
     final case class generatorParams (
       filePath: String,
@@ -11,11 +13,14 @@ object Main {
     
     def main(args: Array[String]): Unit = {
         val rand = new scala.util.Random
+        val t_time = LocalDateTime.now()
+        val t_format = DateTimeFormatter.ofPattern("HH_mm_ss")
+        val t_formattedDate = t_time.format(t_format)
         
         // Create a new file named for args[0]
         // 
         //
-        val p = new PrintWriter(args(0));
+        val p = new PrintWriter("_output/userinfo-%s.csv".format(t_formattedDate));
 
         val f_header = "userName,phoneNumber,socialSecurityNumber,ageBand,loyaltyLevel,emailAddress\n";
         
@@ -29,7 +34,8 @@ object Main {
         //
         // Create the number of rows passed in via args[1] of random user profiles
         //
-        for( i <- 1 to args(1).toInt) {
+        // for( i <- 1 to args(1).toInt) {
+        for( i <- 1 to 5) {
           var f_uname = "username-%05d".format(rand.nextInt(5000));
           var f_phone = "%03d-%03d-%04d".format(rand.nextInt(999), rand.nextInt(99), rand.nextInt(6999));
           var f_ssn = "%03d-%02d-%04d".format(rand.nextInt(399), rand.nextInt(99), rand.nextInt(6999));
